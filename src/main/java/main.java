469cf8e1724194816596f20a777e7fca050cce04
songhee <1586piano@naver.com>
2020-11-28 15:40:42 +0900

    다형성 테스트

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,35 +1,111 @@
+import java.util.Vector;
+
+
+class Product{
+    int price;
+    int bonusPoint;
+
+    Product(int price){
+        this.price = price;
+        bonusPoint = (int)(price/10.0);
+    }
+
+    //위에 int형 매개변수를 받는 생성자를 생성했다.
+    Product(){}
+}
+
+//Product클래스 상속
+class Airpod extends Product{
+    //매개변수 100으로 부모 생성자 Product(int price)실행
+    Airpod(){ super(250); }
+    public String toString() { return "Airpod"; }
+}
+
+class Ipad extends Product{
+    //매개변수 100으로 부모 생성자 Product(int price)실행
+    Ipad(){ super(800); }
+    public String toString() { return "Ipad"; }
+}
+
+class Macbook extends Product{
+    //매개변수 100으로 부모 생성자 Product(int price)실행
+    Macbook(){ super(2000); }
+    public String toString() { return "Macbook"; }
+}
+
+class Buyer{
+    int money = 1500;
+    int bonusPoint = 0;
+    //Product[] item = new Product[10];//구입 물품 보관 배열
+    //int i = 0; //배열 카운트
+
+    //배열은 크기가 한정되어 있으므로 Vector로 관리(동적배열)
+    Vector item = new Vector();
+
+    void buy( Product p ){
+        if( (money+bonusPoint)<p.price ){
+            System.out.println("Not enough money!");
+            return;
+        }
+
+        money-=p.price;
+        bonusPoint+=p.bonusPoint;
+        item.add(p);
+        System.out.println(p+" is yours! ");
+    }
+
+    void summary(){
+        int sum = 0;
+        String itemList = "";
+
+        if(item.isEmpty()){
+            System.out.println("You've nothing");
+            return;
+        }
+
+        for(int i=0; i<item.size(); i++){
+            Product p = (Product)item.get(i);
+            sum += p.price;
+            itemList += (i==0) ? p : ", " + p;
+        }
+
+        System.out.println("You spent " + sum + "dollars");
+        System.out.println("Your items : " + itemList);
+
+    }
+
+    void refund(Product p){
+        if(item.remove(p)){
+            money += p.price;
+            bonusPoint -= p.bonusPoint;
+            System.out.println("You returned " + p);
+        }
+        else{
+            System.out.println("You didn't buy the item");
+        }
+
+    }
+}
+
+
 public class main {
 
     public static void main(String[] args) {
 
-        //부모타입의 참조변수 p를 자식 타입으로 인스턴스 생성
-        Parent p = new Child();
-        Child c = new Child();
+        Buyer songhee = new Buyer();
 
-        //멤버변수는 참조 타입(현재 부모 타입)을 따른다.
-        System.out.println("p.x = "+p.x);
-        //함수는 같은 이름으로 선언한 경우, 오버라이딩한 함수를 실행한다.
-        p.method();
-        System.out.println("c.x = "+c.x);
-        c.method();
-    }
+        //songhee.buy(new Airpod()); f로 하면 벡터 참조변수를 알 수 없기 때문에 아래와 같이 객체 생성
+        Airpod airpod = new Airpod();
+        Ipad ipad = new Ipad();
+        Macbook macbook = new Macbook();
 
-    private static class Parent {
-        String x = "I'm Parent";
-        void method(){
-            System.out.println("Parent");
-            System.out.println("this.x = "+this.x);
-        }
-    }
+        songhee.buy(airpod);
+        songhee.buy(ipad);
+        songhee.buy(macbook);
+        songhee.summary();
 
-    private static class Child extends Parent {
-        String x = "I'm Child";
-        void method(){
-            System.out.println("Child");
-            System.out.println("x = "+x);
-            System.out.println("this.x = "+this.x);
-            System.out.println("super.x = "+super.x);
-        }
+        songhee.refund(ipad);
+        songhee.summary();
     }
 
 }
