0b2be3e1f88404cede879439cc9c871b26b1e8d4
songhee <1586piano@naver.com>
2020-11-28 15:00:16 +0900

    Bingding test

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,35 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello JAVA!");
+
+        //부모타입의 참조변수 p를 자식 타입으로 인스턴스 생성
+        Parent p = new Child();
+        Child c = new Child();
+
+        //멤버변수는 참조 타입(현재 부모 타입)을 따른다.
+        System.out.println("p.x = "+p.x);
+        //함수는 같은 이름으로 선언한 경우, 오버라이딩한 함수를 실행한다.
+        p.method();
+        System.out.println("c.x = "+c.x);
+        c.method();
+    }
+
+    private static class Parent {
+        String x = "I'm Parent";
+        void method(){
+            System.out.println("Parent");
+            System.out.println("this.x = "+this.x);
+        }
+    }
+
+    private static class Child extends Parent {
+        String x = "I'm Child";
+        void method(){
+            System.out.println("Child");
+            System.out.println("x = "+x);
+            System.out.println("this.x = "+this.x);
+            System.out.println("super.x = "+super.x);
+        }
     }
 
 }
