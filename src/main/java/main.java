a614aad76813a11cb031606fbc1a9841d5f7f371
songhee <1586piano@naver.com>
2020-12-21 02:47:35 +0900

    날짜와 시간 비교

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,30 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.chrono.JapaneseDate;
+
 public class main {
 
     public static void main(String[] args) {
+
+        //now()로 생성
+        LocalDate today = LocalDate.now();
+        LocalTime now = LocalTime.now();
+
+        //of()로 지정하여 생성
+        LocalDate kDate = LocalDate.of(1999, 12, 31);
+        JapaneseDate jDate = JapaneseDate.of(1999, 12, 31);
+        LocalTime time = LocalTime.of(23,59,59);
+
+        //equals는 모든 필드가 일치해야 같다고 판단하지만, isEqual은 날짜만 비교한다.
+        System.out.println(kDate.equals(jDate)); //false
+        System.out.println(kDate.isEqual(jDate)); //true
+
+        System.out.println(today.isBefore(kDate)); //false
+        System.out.println(today.isAfter(kDate)); //true
+
+        System.out.println(now.isBefore(time)); //true
+        System.out.println(now.isAfter(time)); //false
+
 
     }
 
