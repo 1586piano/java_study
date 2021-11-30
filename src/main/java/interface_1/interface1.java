656b2956490432ac21a00f0633548d779d543195
song <1586piano@naver.com>
2021-12-01 01:12:25 +0900

    인터페이스 동적 생성

--- src/main/java/interface_1/interface1.java
+++ src/main/java/interface_1/interface1.java
@@ -12,6 +12,7 @@ public class interface1 {
         //B가 없어지고, C가 생긴다면, C로 갈아끼워주면 된다.
         //인터페이스 I는 실제 구현 내용(클래스 B)를 감싸고 있는 껍데기이며, 클래스 A는 껍데기 안애 어떤 내용물이 있는지 몰라도 된다.
         Ai ai = new Ai();
-        ai.methodA(new Bi());
+        //ai.methodA(new Bi()); //매번 이렇게 수동으로 넣어줄 것인가?! InstancaManager클래스에 추가해뒀으니 확인해보자.
+        ai.methodA();
     }
 }
