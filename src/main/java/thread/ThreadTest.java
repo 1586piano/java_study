148d5fd3c453232f7c8ad7371b23613cbc77d5ff
song <1586piano@naver.com>
2021-12-06 00:38:55 +0900

    쓰레드 인터럽트 학습

--- src/main/java/thread/ThreadTest.java
+++ src/main/java/thread/ThreadTest.java
@@ -31,29 +31,29 @@ public class ThreadTest {
         //th1 = new MyThreadExtend(); 와 같이 다시 생성해주어야 한다.
         //왜 run을 실행하지 않고 start를 실행하냐? run은 그냥 메소드이다.
         //start를 실행하면, thread가 작업을 수행하는데 필요한 콜스택을 생성한 다음에 거기에 run을 호출한다. 쓰레드가 종료되면 호출스택을 소멸된다.
-//        th1.start();
-//        th2.start();
+        th1.start();
+        th2.start();
 
         //데몬 쓰레드
         //일반 쓰레드의 보조적인 역할을 수행하는 쓰레드.
         //예를 들어서, 가비지컬렉터, 워드에서 자동저장, 화면 자동갱신 등의 기능을 데몬쓰레드로 구현한다.
-//        Thread damonThread = new Thread(new DamonThread());
-//
-//        //쓰레드를 데몬 쓰레드로 변경한다.
-//        damonThread.setDaemon(true);
-//        damonThread.start();
-//
-//        for(int i=0; i<=10; i++){
-//            try{
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            if(i==5){
-//                DamonThread.autoSave = true;
-//            }
-//        }
-//        System.out.println("시스템이 종료되었습니다.");
+        Thread damonThread = new Thread(new DamonThread());
+
+        //쓰레드를 데몬 쓰레드로 변경한다.
+        damonThread.setDaemon(true);
+        damonThread.start();
+
+        for(int i=0; i<=10; i++){
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(i==5){
+                DamonThread.autoSave = true;
+            }
+        }
+        System.out.println("시스템이 종료되었습니다.");
         //일반 쓰레드가 종료되면, 데몬 쓰레드는 강제적으로 자동 종료된다.
         //만약 데몬쓰레드로 설정하지 않았다면, 수동으로 강제 종료시키기 전에는 쓰레드가 종료되지 않는다.
 
