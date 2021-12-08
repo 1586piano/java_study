42c92c92431a97b90456df7e438fd0aff544b3a9
song <1586piano@naver.com>
2021-12-08 23:57:35 +0900

    checksum 구조수정

--- src/main/java/hashcode/FileChecksum.java
+++ src/main/java/hashcode/FileChecksum.java
@@ -1,6 +1,7 @@
 package hashcode;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileChecksum {
@@ -10,7 +11,10 @@ public class FileChecksum {
     //4. 파일 일치 검사 : 두 파일명을 입력받아서 해시코드를 생성하여 파일 일치 여부를 반환한다.
 
     //파일 생성
-    public static void createFile(TextFile file){
+    public static void createFile(){
+
+        TextFile file = new TextFile();
+
         Scanner scan1 = new Scanner(System.in);
         Scanner scan2 = new Scanner(System.in);
 
@@ -34,6 +38,10 @@ public class FileChecksum {
 
         file.setFileName(fileName);
         file.setFileBody(fileBody);
+
+        System.out.println(">> 입력한 파일 내용입니다.");
+        readFile(file);
+        saveFile(file);
         System.out.println(">> 파일을 생성하였습니다.");
     }
 
@@ -80,26 +88,27 @@ public class FileChecksum {
         return f;
     }
 
-    //파일 일치 검사
-    public static boolean checksum(TextFile f1, TextFile f2){
-        if( (f1.getFileBody().hashCode()) == f2.getFileBody().hashCode() ){
-            System.out.println("두 파일의 내용은 같습니다.");
-        }else {
-            System.out.println("두 파일의 내용은 다릅니다.");
+    //디렉토리 내 파일 전부 오픈
+    public static TextFile openFiles(String fileName) throws IOException {
+        ArrayList<TextFile> fileList = new ArrayList<>();
+        TextFile f = new TextFile();
+        try{
+            FileInputStream fos = new FileInputStream(fileName);
+            DataInputStream dos = new DataInputStream(fos);
+            String fileBody = dos.readUTF();
+            f.setFileName(fileName);
+            f.setFileBody(fileBody);
+        } catch (FileNotFoundException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
         }
-        if( (System.identityHashCode(f1)) == (System.identityHashCode(f2)) ){
-            System.out.println("두 파일은 같습니다.");
-        }else{
-            System.out.println("두 파일은 다릅니다.");
-        }
-        return true;
+        //f가 비어있지 않다면 return
+        return f;
     }
 
-    public static void main(String[] args){
-        TextFile file = new TextFile();
-        createFile(file);
-        readFile(file);
-        saveFile(file);
+    //파일 일치 검사
+    public static boolean checksum(){
 
         TextFile f1 = new TextFile();
         TextFile f2 = new TextFile();
@@ -114,13 +123,32 @@ public class FileChecksum {
             e.printStackTrace();
         }
 
-        readFile(f1);
-        readFile(f2);
-
-        checksum(f1, f2);
-
+        if( (f1.getFileBody().hashCode()) == f2.getFileBody().hashCode() ){
+            System.out.println("두 파일의 내용은 같습니다.");
+        }else {
+            System.out.println("두 파일의 내용은 다릅니다.");
+        }
+        if( (System.identityHashCode(f1)) == (System.identityHashCode(f2)) ){
+            System.out.println("두 파일은 같습니다.");
+        }else{
+            System.out.println("두 파일은 다릅니다.");
+        }
+        return true;
     }
 
+    public static void main(String[] args){
+
+        System.out.println("작업 번호를 입력하시오 : ");
+        Scanner scan = new Scanner(System.in);
+        switch (scan.nextInt()){
+            case 1: createFile();
+                    break;
+            case 2: checksum();
+                    break;
+            default: System.out.println("작업 번호를 입력하시오 : ");
+                    break;
+        }
+    }
 }
 
 
