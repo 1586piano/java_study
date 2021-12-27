6919de8ac0427e22183f13de6c877d8a90c51c01
song <1586piano@naver.com>
2021-12-28 00:07:43 +0900

    직렬화 학습

--- src/main/java/serial/SerialEx2.java
+++ src/main/java/serial/SerialEx2.java
@@ -1,44 +1,59 @@
 package serial;
 
+import interface_1.A;
+
 import java.io.*;
+import java.util.ArrayList;
 
 public class SerialEx2 {
     public static void main(String[] args) throws Exception {
 
-        Student student = new Student("JS", 123);
-
+        String fileName = "test.ser";
         // serialization
-        TextFile file = new TextFile();
-        file.setFileName("test");
-        file.setFileBody("testFile");
-        try (FileOutputStream fos = new FileOutputStream(String.valueOf(file));
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(student);
+        TextFile file1 = new TextFile();
+        //확장자는 .ser로 하는 것이 보통이지만, 제약은 없다.
+        file1.setFileName("test1");
+        file1.setFileBody("testFile1");
+        //직렬화되지 않기  때문에 역직렬화하면, null이 된다.
+        file1.setPassword("5678");
+
+        TextFile file2 = new TextFile();
+        file2.setFileName("test2");
+        file2.setFileBody("testFile2");
+        file2.setPassword("5678");
+
+        ArrayList<TextFile> arrFile = new ArrayList<>();
+        arrFile.add(file1);
+        arrFile.add(file2);
+
+        try (FileOutputStream fos = new FileOutputStream(fileName);
+             BufferedOutputStream bos = new BufferedOutputStream(fos);
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+            oos.writeObject(file1);
+            oos.writeObject(file2);
+            oos.writeObject(arrFile);
             oos.flush();
         }
 
         // deserialization
-        Student result = null;
-        try (FileInputStream fis = new FileInputStream(String.valueOf(file));
-             ObjectInputStream ois = new ObjectInputStream(fis)) {
-            result = (Student) ois.readObject();
-        }
-
-        System.out.println(result.toString());
-    }
-
-    public static class Student implements Serializable {
-        private String name;
-        private int id;
-
-        Student(String name, int id) {
-            this.name = name;
-            this.id = id;
-        }
-
-        @Override
-        public String toString() {
-            return "name : " + name + ", id : " + id;
+        ArrayList result = null;
+        TextFile resFile1 = null;
+        TextFile resFile2 = null;
+        try (FileInputStream fis = new FileInputStream(fileName);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             ObjectInputStream ois = new ObjectInputStream(bis)) {
+            //readObjec의 반환 타입이 Object이기 때문에 형변환이 필요하다.
+            //역직렬화하여 객체를 읽을 때는, 직렬화한 순서와 일치해야한다.
+            //역직렬화할 때는 직려화 했을 때와 같은 클래스를 사용해야 한다.
+            //클래스 이름이 같더라도 클래스의 내용이 변경되면 역직렬화가 실패하므로, 수동으로 클래스 UID를 지정할 수도 있다. (944p 참고)
+            resFile1 = (TextFile) ois.readObject();
+            resFile2 = (TextFile) ois.readObject();
+            result = (ArrayList) ois.readObject();
+            System.out.println(resFile1.toString());
+            System.out.println(resFile2.toString());
+            System.out.println(result.toString());
+            ois.close();
         }
     }
+
 }
--- src/main/java/serial/TextFile.java
+++ src/main/java/serial/TextFile.java
@@ -1,12 +1,19 @@
 package serial;
 
-class TextFile {
+import java.io.Serializable;
+
+class TextFile implements Serializable {
+
     private String fileName;
     private String fileBody;
+    //password와 같이 보안상 직렬화가 되면 안되는 값의 경우 transient를 붙인다.
+    //transient가 붙은 인스턴스변수의 값은 그 타입의 기본값으로 직렬화된다.
+    transient private String password;
 
     TextFile(){
         this.fileName = "제목없음";
         this.fileBody = "내용없음";
+        this.password = "1234";
     }
 
     public void setFileName(String fileName){
@@ -17,12 +24,24 @@ class TextFile {
         this.fileBody = fileBody;
     }
 
+    public void setPassword(String password){
+        this.password = password;
+    }
+
     public String getFileName(){
         return this.fileName.toString();
     }
 
     public String getFileBody(){
         return this.fileBody.toString();
+    }
+
+    public String getPassword(){ return this.password.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "fileName : " + this.fileName + ", fileBody : " + this.fileBody + ", pw : " + this.password;
     }
 
 }
